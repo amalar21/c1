24ae4c009b35bb7be5567e2517379f74c97cb08e
Amala <amalar0521@gmail.com>
2024-06-30 18:59:32 +0530

    first commit

---
+++ project2/src/project02/Project3.java
@@ -0,0 +1,13 @@
+package project02;
+
+public class Project3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int A = 10;
+int B = 20;
+System.out.println("A+B");
+//System.out.println("A+B");
+	}
+
+}
